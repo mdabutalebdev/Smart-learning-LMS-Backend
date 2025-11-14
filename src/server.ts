3a9b25c1ad11fa7262c06adfc886e1104fb6485c
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-11-14 23:40:36 +0600

    basic setup

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+ 
+import mongoose from "mongoose"
+import app from "./index.js";
+main().catch(err => console.log(err));
+const port = 5000
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  
+}
