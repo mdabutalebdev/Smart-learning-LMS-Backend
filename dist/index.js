d0e25eec2fd8376f1b0447c06eee42452960721f
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-11-16 20:47:01 +0600

    env setup

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World! Iam a MERN Stack Devekoper');
+app.use(cors());
+dotenv.config();
+app.get("/", (req, res) => {
+    res.send("Hello World! Iam a MERN Stack Devekoper");
 });
 export default app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 const app: Application = express();
-
+app.use(cors());
+dotenv.config();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! Iam a MERN Stack Devekoper");
 });
